c7f1066a947b5a953fc421af1deb790666e2f2a7
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-18 22:25:28 +0900

    [Silver II] Title: A → B, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,16 @@
+A, B = map(int, input().split())
+
+result = 1
+while True:
+    if B % 2 == 1:
+        B = (B - 1) / 10
+        result += 1
+    else:
+        B /= 2
+        result += 1
+    if B == A:
+        break
+    elif B < A:
+        result = -1
+        break
+print(result)
