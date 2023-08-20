cac51e71ebbe953423514ee0ce1149a4ab5ae5da
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-21 02:56:11 +0900

    [Silver II] Title: 유기농 배추, Time: 300 ms, Memory: 31556 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(i, j):
+    if 0 <= i < N and 0 <= j < M:
+        if box[i][j] == 1:
+            box[i][j] = 0
+            dfs(i-1, j)
+            dfs(i, j-1)
+            dfs(i+1, j)
+            dfs(i, j+1)
+            return True
+        return False
+    else:
+        return False
+
+T = int(input())
+
+for _ in range(T):
+    M, N, K = map(int, input().split())  # 가로 / 세로 / 배추위치개수
+    box = [[0]*M for _ in range(N)]
+    for _ in range(K):
+        b, a = map(int, input().split())  # 가로, 세로 -> box[a][b]
+        box[a][b] = 1
+
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            if dfs(i, j) == True:
+                result += 1
+
+    print(result)
