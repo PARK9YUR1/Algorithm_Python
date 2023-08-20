44341f3dfe075329d41df08f15c60353e1afdd37
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-20 19:47:11 +0900

    [Silver V] Title: 단어 정렬, Time: 1072 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,18 +1,13 @@
 N = int(input())
-words = [[] for _ in range(51)]
-mx_len = 0
+words = []
 
 for _ in range(N):
     word = input()
+    words.append(word)
 
-    if word not in words[len(word)]:
-        words[len(word)].append(word)
-        words[len(word)].sort()
+words = list(set(words))
+words.sort()
+words.sort(key=len)
 
-    if mx_len < len(word):
-        mx_len = len(word)
-
-for i in range(1, mx_len + 1):
-    if len(words[i]) > 0:
-        for j in range(len(words[i])):
-            print(words[i][j])
+for w in words:
+    print(w)
