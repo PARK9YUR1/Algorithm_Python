c16d94e2a7fe80c9239198902eb7a9a088944513
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-20 19:36:09 +0900

    [Silver V] Title: 단어 정렬, Time: 6264 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,15 +1,12 @@
 N = int(input())
 words = [[] for _ in range(51)]
-check_list = []
 
 for _ in range(N):
     word = input()
-    if word in check_list:
-        continue
-    check_list.append(word)
 
-    words[len(word)].append(word)
-    words[len(word)].sort()
+    if word not in words[len(word)]:
+        words[len(word)].append(word)
+        words[len(word)].sort()
 
 for i in range(len(words)):
     if len(words[i]) > 0:
