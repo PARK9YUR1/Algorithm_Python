7e0ab9fe2c9606fbb386bcce5a182ced857a4974
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-23 07:40:10 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 752 ms, Memory: 84248 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+def dfs(x):
+    global visited, cnt
+    lst[x] = cnt
+    cnt += 1
+    visited[x] = True
+    for k in graphs[x]:
+        if visited[k] == True:  # if visited[k] == False: dfs(k) 안됨
+            continue
+        dfs(k)
+
+
+# 정점 수, 간선 수, 시작 정점
+N, M, R = map(int, input().split())
+adj = []
+visited = [[False] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    adj.append((a, b))
+
+graphs = [[] for _ in range(N+1)]
+for i in range(len(adj)):
+    u, v = adj[i]
+    graphs[u].append(v)
+    graphs[v].append(u)
+
+for g in graphs:
+    g.sort(reverse=True)
+
+cnt = 1
+lst = [0] * (N+1)
+dfs(R)
+for l in range(1, N+1):
+    print(lst[l])
