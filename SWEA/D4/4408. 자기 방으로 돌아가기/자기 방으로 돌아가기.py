377d415a8e42d8222f02c15ae0cb4e0c8f474513
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-24 21:06:06 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 180 ms, Memory: 61,220 KB -BaekjoonHub

--- SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -1,15 +1,13 @@
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())  # 돌아가야 할 학생들의 수
-    box = [0] * 201
+    lst = [0] * 201
+    N = int(input())  # 돌아가야 할 학생 수
     for _ in range(N):
-        S, E = map(int, input().split())
-        if S > E:
-            S, E = E, S
-        S = S//2 + S%2
-        E = E//2 + E%2
-        for i in range(S, E+1):
-            box[i] += 1
+        a, b = map(int, input().split())
+        if a > b:
+            a, b = b, a
+        for i in range((a+1)//2, (b+1)//2+1):
+            lst[i] += 1
 
-    result = max(box)
+    result = max(lst)
     print(f'#{tc} {result}')
