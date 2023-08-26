f0e21e9cc2c952e4e64c381a427e5c4bcff4e55f
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-27 01:07:05 +0900

    [Silver V] Title: 종이자르기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -0,0 +1,30 @@
+A, B = map(int, input().split())  # 가로, 세로 길이
+cut = int(input())  # 잘라야하는 점선 개수
+box = [[0]*A for _ in range(B)]
+G = [0, B]  # 가로방향
+S = [0, A]  # 세로방향
+for _ in range(cut):
+    dir, num = map(int, input().split())  # 가로방향(0)or세로방향(1), 점선 번호
+    if dir == 0:
+        G.append(num)
+    else:
+        S.append(num)
+
+G.sort()
+S.sort()
+
+G_mx = 0
+S_mx = 0
+
+for i in range(len(G)-1):
+    ans = G[i+1] - G[i]
+    if G_mx < ans:
+        G_mx = ans
+
+for i in range(len(S)-1):
+    ans = S[i+1] - S[i]
+    if S_mx < ans:
+        S_mx = ans
+
+result = G_mx * S_mx
+print(result)
