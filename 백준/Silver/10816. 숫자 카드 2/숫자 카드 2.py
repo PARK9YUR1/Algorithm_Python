44965814170d3125dad7649da6a1ce4da1fd9f45
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-30 23:22:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 796 ms, Memory: 124952 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -10,9 +10,7 @@ for num in input().split():  # 상근이 숫자 카드
         card[num] = 1
 
 M = int(input())
-find = list(map(int, input().split()))  # 찾아야 할 카드
-
-for cd in find:
+for cd in input().split():  # 찾아야 할 카드
     cd = str(cd)
     if cd in card:
         print(card[cd], end=' ')
