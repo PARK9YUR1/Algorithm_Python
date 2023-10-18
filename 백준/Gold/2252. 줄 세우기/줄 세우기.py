e6dcad00f6d6061c66695e03a4e912bd6f41c855
rigoood <132658372+rigoood@users.noreply.github.com>
2023-10-19 08:01:47 +0900

    [Gold III] Title: 줄 세우기, Time: 5712 ms, Memory: 120736 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = [[] for _ in range(N+1)]
+arr = [0]*(N+1)
+visited = [False]*(N+1)
+result = []
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    nums[a].append(b)
+    arr[b] += 1
+
+q = deque()
+for i in range(1, N+1):
+    if arr[i] == 0:
+        q.append(i)
+        visited[i] = True
+
+while q:
+    x = q.popleft()
+    result.append(x)
+    for i in nums[x]:
+        arr[i] -= 1
+    for i in range(1, N+1):
+        if arr[i] == 0 and visited[i] == False:
+            q.append(i)
+            visited[i] = True
+
+print(*result)
