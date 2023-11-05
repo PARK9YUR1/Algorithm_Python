09417079cf7187a41e862f1590729c22bf76d1a5
rigoood <132658372+rigoood@users.noreply.github.com>
2023-11-06 00:38:29 +0900

    [Gold IV] Title: 미친 로봇, Time: 480 ms, Memory: 114224 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.py
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.py
@@ -1,24 +1,25 @@
 arr = list(map(int, input().split()))
+# 이동횟수, 확률
 N, dir = arr[0], [arr[i]/100 for i in range(1, 5)]
 visited = [[False]*29 for _ in range(29)]
-ans = 0
+result = 0
 
 #     동 서  남 북
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-# x좌표, y좌표, 이동횟수, 확률
+# [x][y], 이동횟수, 확률
 def dfs(x, y, move, p):
-    global ans
+    global result
     visited[x][y] = True
     for k in range(4):
         nx, ny = x + dx[k], y + dy[k]
         if visited[nx][ny] == False:
             if move == N:  # N번 이동한 경우
-                ans += p * dir[k]  # 확률을 더함
+                result += p * dir[k]  # 확률을 더함
             else:
                 dfs(nx, ny, move+1, p * dir[k])
     visited[x][y] = False
 
 dfs(14, 14, 1, 1)  # 시작점 : 중앙
-print(ans)
+print(result)
