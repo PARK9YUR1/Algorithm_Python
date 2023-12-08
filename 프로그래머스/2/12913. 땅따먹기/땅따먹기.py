0b4667b67363b46c3010145fc1137f8e8b791062
rigoood <132658372+rigoood@users.noreply.github.com>
2023-12-09 04:10:36 +0900

    [level 2] Title: 땅따먹기, Time: 220.68 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,12 +1,17 @@
 def solution(land):
     N = len(land)
-    DP = [land[0] + [0]*3 for _ in range(N)]
-    # DP = [[0]*4 for _ in range(N)]
-    # DP[0] = land[0]
-    
-    answer = 0
+    DP = [[0]*4 for _ in range(N)]
+    DP[0] = land[0]
     
     for i in range(1, N):
+        # for j in range(4):
+        #     # DP[i][j] = max(DP[i-1][:j], DP[i-1][j+1:]) + land[i][j]
+        #     arr = []
+        #     for k in range(4):
+        #         if j != k:
+        #             arr.append(DP[i-1][k])
+        #     DP[i][j] = max(arr) + land[i][j]
+            
         DP[i][0] = max(DP[i-1][1], DP[i-1][2], DP[i-1][3]) + land[i][0]
         DP[i][1] = max(DP[i-1][0], DP[i-1][2], DP[i-1][3]) + land[i][1]
         DP[i][2] = max(DP[i-1][0], DP[i-1][1], DP[i-1][3]) + land[i][2]
