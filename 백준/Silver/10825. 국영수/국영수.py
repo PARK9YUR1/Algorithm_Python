f4c637c491c76f10ee79518858ed563b0a6dfbf4
rigoood <132658372+rigoood@users.noreply.github.com>
2023-12-15 00:11:36 +0900

    [Silver IV] Title: 국영수, Time: 496 ms, Memory: 152524 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,16 +1,7 @@
 # 10825
 N = int(input())
-grade = []
-for _ in range(N):
-    name, kor, eng, math = input().split()
-    arr = []
-    for n in name:
-        arr.append(ord(n))
-    grade.append([arr, int(kor), int(eng), int(math)])
-
-grade.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
-for x in grade:
-    name = ''
-    for num in x[0]:
-        name += chr(num)
+arr = [input().split() for _ in range(N)]
+arr.sort(key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+for x in arr:
+    name = x[0]
     print(name)
