8c4fc8d6c28c70edf10e639cadde947d69f915d1
rigoood <132658372+rigoood@users.noreply.github.com>
2023-12-16 15:05:59 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -0,0 +1,22 @@
+# 16173
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [[False]*N for _ in range(N)]
+
+def dfs(x, y):
+    visited[x][y] = True
+    if visited[N-1][N-1] == True:
+        return
+
+    S = arr[x][y]
+
+    for dx, dy in (0, S), (S, 0):
+        nx, ny = x+dx, y+dy
+        if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == False:
+            dfs(nx, ny)
+
+dfs(0, 0)
+if visited[N-1][N-1] == True:
+    print('HaruHaru')
+else:
+    print('Hing')
