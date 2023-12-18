0e03438a29a9843089e056914233b1d1aa29fffd
rigoood <132658372+rigoood@users.noreply.github.com>
2023-12-18 21:58:45 +0900

    [level 2] Title: 과제 진행하기, Time: 1.52 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/README.md
+++ 프로그래머스/2/176962. 과제 진행하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 과제 진행하기 - 176962 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/176962#) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 2.87 ms
+메모리: 10.7 MB, 시간: 1.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 21:53:57
+2023년 12월 1일 21:58:43
 
 ### 문제 설명
 
--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,42 +1,37 @@
 def solution(plans):
-    N = len(plans)
-    plans.sort(key=lambda x: x[1])  # 시작시간 순 정렬
-    for i in range(N):
-        start, end = plans[i][1], plans[i][2]
-        minute = int(start[3:]) + int(end)
-        h = f'{minute // 60 + int(start[:2])}'.zfill(2)
-        m = f'{minute % 60}'.zfill(2)
-        plans[i][2] = f'{h}:{m}'
-
-    stack = []
     result = []
-
+    
+    N = len(plans)
     for i in range(N):
-        if stack:
-            sj, S, E = stack[-1]
-            subject, start, end = plans[i]
-            print('i E start', i, E, start)
-            if E <= start:
-                result.append(sj)
-                stack.pop()
+        hour, minute = map(int, plans[i][1].split(':'))
+        plans[i][1] = hour * 60 + minute
+        plans[i][2] = int(plans[i][2])
+    plans.sort(key=lambda x: x[1])  # 시작시간 순 정렬
+    
+    stack = []
+    cur_time = plans[0][1]  # 현재 시간
+    for i in range(N):
+        while stack:
+            cur_sj, cur_start, cur_task = plans[i]  # 다음과제
+            subject, start, task = stack[-1]  # 이전과제
+            
+            cur_time = max(cur_time, start)  # 현재시간 : 현재시간과 이전과제 시작시간 중 큰 값
+            end = cur_time + task  # 끝나는시간 : 현재시간 + 이전과제 소요시간
 
-                if i == N-1:
-                    result.append(subject)
-                    while stack:
-                        x = stack.pop()
-                        result.append(x[0])
-                else:
-                    while True:
-                        if stack:
-                            sj, S, E = stack[-1]
-                            if E <= start:
-                                result.append(sj)
-                                stack.pop()
-                            else:
-                                break
-                        else:
-                            break
+            if end > cur_start:  # 이전과제 끝나는 시간이 다음과제 시작시간보다 늦은 경우
+                x = cur_start - cur_time  # 남은시간 : 다음과제 시작시간 - 현재시간
+                stack[-1][2] = task - x  # 이전과제 소요시간 : 기존 소요시간 - 남은시간
+                cur_time += x  # 현재시간 : 현재시간 + 남은시간
+                break
+            else:  # 이전과제 끝나는 시간이 다음과제 시작시간과 같거나 빠른경우
+                stack.pop()  # 이전과제를 끝냄
+                result.append(subject)  # 끝낸과제 담기
+                cur_time += task  # 현재시간 : 현재시간 + 과제소요시간
+                
         stack.append(plans[i])
 
-    print(plans)
+    
+    for i in range(len(stack)):  # stack에 남은 과제들 거꾸로 담아주기
+        subject, s, t = stack.pop()
+        result.append(subject)
     return result
