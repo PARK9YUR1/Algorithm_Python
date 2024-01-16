c340799f617016f8a791765c1cb477e6c81a60d6
rigoood <132658372+rigoood@users.noreply.github.com>
2024-01-16 09:18:18 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,37 @@
+def solution(begin, target, words):
+    answer = 0
+    words.insert(0, begin)
+    N, W = len(words), len(begin)
+    if target in words:
+        idx = words.index(target)
+        graph = [[] for _ in range(N)]
+        
+        for i in range(N):
+            for j in range(N):
+                if i == j:
+                    continue
+                arr = [words[i][k] == words[j][k] for k in range(W)]
+                if sum(arr) == W-1:
+                    graph[i].append(j)
+        
+        def bfs(v):
+            nonlocal answer
+            q = [v]
+            visited = [0]*N
+            visited[v] = 1
+            
+            while q:
+                x = q.pop(0)
+                
+                if x == idx:
+                    answer = visited[x] - 1
+                    return
+                
+                for u in graph[x]:
+                    if not visited[u]:
+                        q.append(u)
+                        visited[u] = visited[x] + 1
+
+        bfs(0)
+        
+    return answer
