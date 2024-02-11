43c2bd35f53b6e0fe6de4dccc68d46a1ea61d9ae
rigoood <132658372+rigoood@users.noreply.github.com>
2024-02-11 22:39:26 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -2,20 +2,14 @@ arr = [int(input()) for _ in range(9)]
 arr.sort()
 
 x = sum(arr) - 100
-a = b = 0
 
-for i in range(9):
-    for j in range(i+1, 9):
-        if a or b:
-            break
-        if arr[i] + arr[j] == x:
-            a, b = i, j
-            break
-
-if b < 9:
-    result = arr[:a] + arr[a+1:b] + arr[b+1:]
-else:
-    result = arr[:a] + arr[a+1:]
+def func():
+    for i in range(9):
+        for j in range(i+1, 9):
+            if arr[i] + arr[j] == x:
+                return arr[:i] + arr[i+1:j] + arr[j+1:]
+    
+result = func()
 
 for res in result:
     print(res)
